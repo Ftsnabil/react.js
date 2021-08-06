7234f25b72387c0eed9df7fde872312d0c0e44f5
Ftsnabil <nabfts116@gmail.com>
2021-08-06 10:40:53 +0100

    first commit

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,44 +1,27 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form, Button, Navbar, Container, Nav} from 'react-bootstrap';
+import './index.css';
+import './style.css';
+
+import image from './image.jpg';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="primary" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-      <header className="App-header">
-        <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+    
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title red">Fatnassi Nabil</h1>
+    <br />
+    <div className="imageClass">
+    <img src={image} alt="imageinsrc" />
+    <br />
+    <img src="/imageInPublic.jpg" alt="imageinpublic" />
+    </div>
+  </div>
+  <video width={320} height={240} controls>
+    <source src="/Video.mp4" type="video/mp4" />
+  </video>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-     
-      </header>
     </div>
   );
 }
